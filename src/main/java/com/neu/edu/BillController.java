2936a1c5816ee0ce282d3a5876d54fe7b5ef9406
Prerna Sharma <sharma.prer@northeastern.edu>
2020-01-29 19:09:03 -0500

    Updated branch filter and exception handling

--- src/main/java/com/neu/edu/BillController.java
+++ src/main/java/com/neu/edu/BillController.java
@@ -23,6 +23,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.neu.edu.dao.BillDao;
 import com.neu.edu.dao.UserDao;
+import com.neu.edu.exception.QueriesException;
 import com.neu.edu.pojo.Bill;
 import com.neu.edu.pojo.BillDbEntity;
 import com.neu.edu.pojo.User;
@@ -40,7 +41,7 @@ public class BillController {
 	
 	@PostMapping(path ="/v1/bill/", produces=MediaType.APPLICATION_JSON_VALUE, consumes=MediaType.APPLICATION_JSON_VALUE)
 	public ResponseEntity<?> createBills(@RequestHeader(value = "Authorization", required=true) String authToken, 
-										 @Valid @RequestBody(required=true) Bill bill){
+										 @Valid @RequestBody(required=true) Bill bill) throws QueriesException{
 									
 			User userExists = checkAuthentication(authToken);
 			
@@ -55,19 +56,15 @@ public class BillController {
 				try {
 					BillDbEntity billDB= billDao.save(billDb);
 					return new ResponseEntity<>(billDB,HttpStatus.CREATED);
-				}
-				catch(Exception e) {
-					e.printStackTrace();
-					
-				}
-			}
-			
-	  return new ResponseEntity<>("Check username or password",HttpStatus.BAD_REQUEST);
-			
-	 }
+				}catch(Exception e) {
+				 throw new QueriesException("Internal SQL Server Error");
+			   }
+		    }
+	   return new ResponseEntity<>("{\n" + "\"error\": \"User does not exist\"\n" + "}",HttpStatus.BAD_REQUEST);
+	}
 	
 	@GetMapping(path ="/v1/bills", produces=MediaType.APPLICATION_JSON_VALUE)
-	public ResponseEntity<?> getAllBills(@RequestHeader(value = "Authorization", required=true) String authToken){
+	public ResponseEntity<?> getAllBills(@RequestHeader(value = "Authorization", required=true) String authToken)throws QueriesException{
 		
 		User userExists = checkAuthentication(authToken);
 		
@@ -76,52 +73,48 @@ public class BillController {
 			try {
 				List<BillDbEntity> billList = billDao.getBillsByOwnerId(userExists.getId());
 				return new ResponseEntity<>(billList,HttpStatus.OK);
-			}
-			catch(Exception e) {
-				e.printStackTrace();
+			}catch(Exception e) {
+				throw new QueriesException("Internal SQL Server Error");
 			}
 			
 		} 
-		return new ResponseEntity<>("Check username or password",HttpStatus.BAD_REQUEST);
+		return new ResponseEntity<>("{\n" + "\"error\": \"Check username or password\"\n" + "}",HttpStatus.BAD_REQUEST);
+		
 	 }
 	
 	@DeleteMapping(path ="/v1/bill/{id}", produces=MediaType.APPLICATION_JSON_VALUE)
-	public ResponseEntity<?> deleteBillbyId(@RequestHeader(value = "Authorization",required=true) String authToken, 
-											@PathVariable(required=true)String id){
+	public ResponseEntity<Object> deleteBillbyId(@RequestHeader(value = "Authorization",required=true) String authToken, 
+											     @PathVariable(required=true)String id) throws QueriesException{
 		
 		User userExists = checkAuthentication(authToken);
 		try {
 			Optional<BillDbEntity> billDbEntityOpt = billDao.findById(id);
 			if(userExists != null) { //isAuthorisedUser
-				
-				//if (billDao.existsById(id)) { //Bill id is valid
-				if (billDbEntityOpt.isPresent()) {
+
+				if (billDbEntityOpt.isPresent()) { //Bill id is valid
 					
 					BillDbEntity billEntity = billDbEntityOpt.get();
-					
-					//if(billDao.getInfo(userExists.getId(), id) != null) {
 					
 					if(billEntity.getOwner_id().equals(userExists.getId())) { //Bill has valid owner
 						
 						billDao.deleteById(id);
-						return new ResponseEntity<>("Bill deleted",HttpStatus.NO_CONTENT);
+						return new ResponseEntity<>(HttpStatus.NO_CONTENT);
 					}
 					else
-						return new ResponseEntity<>("Not authorized to delete this bill ID ",HttpStatus.UNAUTHORIZED);
+						return new ResponseEntity<>("{\n" + "\"error\":\"Not authorized to delete this bill ID\"\n" + "}",HttpStatus.UNAUTHORIZED);
 				}
 				else
-					return new ResponseEntity<>("Bill Not Found",HttpStatus.NOT_FOUND);
+					return new ResponseEntity<>("{\n" + "\"error\":\"Bill Not Found\"\n" + "}",HttpStatus.NOT_FOUND);
 			}
-		}
-		catch(Exception e) {
-			e.printStackTrace();
+		}catch(Exception e) {
+			throw new QueriesException("Internal SQL Server Error");
 		} 
-		return new ResponseEntity<>("Username or password Invalid",HttpStatus.UNAUTHORIZED);
+		return new ResponseEntity<>("{\n" + "\"error\":\"Not authenticated\"\n" + "}",HttpStatus.UNAUTHORIZED);
 	}
 	
 	@GetMapping(path ="/v1/bill/{id}", produces=MediaType.APPLICATION_JSON_VALUE)
 	public ResponseEntity<?> getBillById(@RequestHeader(value = "Authorization",required=true) String authToken, 
-										 @PathVariable(required=true)String id){
+										 @PathVariable(required=true)String id) throws QueriesException{
 		
 		User userExists = checkAuthentication(authToken);
 		
@@ -130,34 +123,30 @@ public class BillController {
 			
 			if(userExists != null) { //isAuthorisedUser
 				
-				//if (billDao.existsById(id)) { 
 				if (billDbEntityOpt.isPresent()) { //Bill id is valid
+					
 					BillDbEntity billEntity = billDbEntityOpt.get();
 					
-					//if(billDao.getInfo(userExists.getId(), id) != null) {
 					if(billEntity.getOwner_id().equals(userExists.getId())) { //Bill has valid owner
-						
 						BillDbEntity billById = billDao.getOne(id);
 						return new ResponseEntity<>(billById,HttpStatus.OK);
 					}
 					else
-						return new ResponseEntity<>("Not authorized to view this bill ID ",HttpStatus.UNAUTHORIZED);		
+						return new ResponseEntity<>("{\n" + "\"error\":\"Not authorized to view this bill ID\"\n" + "}",HttpStatus.UNAUTHORIZED);		
 				}
 				else
-					return new ResponseEntity<>("Bill ID Not Found",HttpStatus.NOT_FOUND);
+					return new ResponseEntity<>("{\n" + "\"error\":\"Bill Not Found\"\n" + "}",HttpStatus.NOT_FOUND);
 			}
-		}
-		catch (Exception e) {
-			e.printStackTrace();
-			
+		}catch (Exception e) {
+			throw new QueriesException("Internal SQL Server Error");
 		} 
-	    return new ResponseEntity<>("Username or password is Incorrect",HttpStatus.UNAUTHORIZED);
+	    return new ResponseEntity<>("{\n" + "\"error\":\"Not authenticated\"\n" + "}",HttpStatus.UNAUTHORIZED);
 	}
 	
 	@PutMapping(path ="/v1/bill/{id}", produces=MediaType.APPLICATION_JSON_VALUE,consumes=MediaType.APPLICATION_JSON_VALUE)
 	public ResponseEntity<?> upadateBillbyId(@RequestHeader(value = "Authorization",required=true) String authToken, 
 											 @PathVariable(required=true)String id,
-											 @Valid @RequestBody(required=true) Bill bill){
+											 @Valid @RequestBody(required=true) Bill bill) throws QueriesException{
 	 
 	  if(bill.getOwner_id() == null && bill.getId()==null && bill.getCreated_ts()==null && bill.getUpdated_ts()==null) { 
 		  
@@ -188,26 +177,25 @@ public class BillController {
 							return new ResponseEntity<>(billEntity,HttpStatus.OK);
 						}
 						
-						return new ResponseEntity<>("Not updated",HttpStatus.BAD_REQUEST);
+						return new ResponseEntity<>("{\n" + "\"error\":\"Not updated\"\n" + "}",HttpStatus.BAD_REQUEST);
 						
 					} else
-						return new ResponseEntity<>("Not authorized to view this bill ID ",HttpStatus.UNAUTHORIZED);
-					
+						return new ResponseEntity<>("{\n" + "\"error\":\"Not authorized to view this bill ID\"\n" + "}",HttpStatus.UNAUTHORIZED);
 				}
 				else
-					return new ResponseEntity<>("Bill ID Not Found",HttpStatus.NOT_FOUND);
+					return new ResponseEntity<>("{\n" + "\"error\":\"Bill Not Found\"\n" + "}",HttpStatus.NOT_FOUND);
 			}
-		}
-		catch (Exception e) {
-			e.printStackTrace();		} 
-		return new ResponseEntity<>("Username or password is Incorrect",HttpStatus.UNAUTHORIZED);
+		}catch (Exception e) {
+			throw new QueriesException("Internal SQL Server Error");	
+		} 
+		return new ResponseEntity<>("{\n" + "\"error\":\"Not authorized\"\n" + "}",HttpStatus.UNAUTHORIZED);
 	  }
 	  
-	  return new ResponseEntity<>("Do not provide id,created_ts,updated_ts, and owner_id fields",HttpStatus.BAD_REQUEST);
+	  return new ResponseEntity<>("{\n" + "\"error\":\"Do not provide id,created_ts,updated_ts, and owner_id fields\"\n" + "}",HttpStatus.BAD_REQUEST);
 			
 	 }
 	
-	 public User checkAuthentication(String authToken) {
+	 public User checkAuthentication(String authToken) throws QueriesException {
 
 			// Remove BASIC from auth-token value
 			String[] splitToken = authToken.split("\\s+");
@@ -223,14 +211,14 @@ public class BillController {
 			String passwordToken = splitStringTok[1];
 			
 			try {
-				
 				User usernameExists = userDao.emailExists(email_addressToken);
 				if(usernameExists != null && BCrypt.checkpw(passwordToken, usernameExists.getPassword())) {
 					return usernameExists;
 				}
+			
 			}
-			catch(Exception e) {
-				e.printStackTrace();
+			catch (Exception e) {
+				throw new QueriesException("Internal SQL Server Error");
 			}
 			return null;
 	 }
---
+++ src/main/java/com/neu/edu/exception/ExceptionResponse.java
@@ -0,0 +1,43 @@
+package com.neu.edu.exception;
+
+import java.time.LocalDateTime;
+
+import org.springframework.http.HttpStatus;
+
+public class ExceptionResponse {
+	
+	private String message;
+	private String path;
+	private HttpStatus status;
+	private LocalDateTime timestamp;
+	
+	public String getMessage() {
+		return message;
+	}
+	public void setMessage(String message) {
+		this.message = message;
+	}
+	public String getPath() {
+		return path;
+	}
+	public void setPath(String path) {
+		this.path = path;
+	}
+	
+	public HttpStatus getStatus() {
+		return status;
+	}
+	public void setStatus(HttpStatus status) {
+		this.status = status;
+	}
+	public LocalDateTime getTimestamp() {
+		return timestamp;
+	}
+	public void setTimestamp(LocalDateTime timestamp) {
+		this.timestamp = timestamp;
+	}
+	
+	
+	
+
+}
