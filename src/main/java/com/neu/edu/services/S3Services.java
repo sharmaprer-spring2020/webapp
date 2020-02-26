bee305ba90179444854eeea712d23ac72a5c1874
Prerna Sharma <sharma.prer@northeastern.edu>
2020-02-25 21:47:44 -0500

    Updating for S3

---
+++ src/main/java/com/neu/edu/services/S3Services.java
@@ -0,0 +1,41 @@
+package com.neu.edu.services;
+
+import org.springframework.stereotype.Component;
+
+import software.amazon.awssdk.core.sync.RequestBody;
+import software.amazon.awssdk.regions.Region;
+import software.amazon.awssdk.services.s3.S3Client;
+import software.amazon.awssdk.services.s3.model.DeleteObjectRequest;
+import software.amazon.awssdk.services.s3.model.PutObjectRequest;
+
+@Component
+public class S3Services {
+	
+	private static S3Client s3 = S3Client.builder().region(Region.US_EAST_1).build();
+	private static final String BUCKET_NAME = System.getenv("BUCKET_NAME");
+	public boolean addFile(byte[] fileBytes, String fileName){
+		try {
+			System.out.println(fileName);
+			System.out.println(BUCKET_NAME);
+			s3.putObject(PutObjectRequest.builder().bucket(BUCKET_NAME).key(fileName)
+	                .build(),
+	                RequestBody.fromBytes(fileBytes));
+			return true;
+		} catch(Exception e) {
+			e.printStackTrace();
+			return false;
+		}
+	}
+	
+	public boolean deleteFile(String fileName){
+		try {
+			System.out.println(fileName);
+			DeleteObjectRequest deleteObjectRequest = DeleteObjectRequest.builder().bucket(BUCKET_NAME).key(fileName).build();
+	        s3.deleteObject(deleteObjectRequest);
+			return true;
+		} catch(Exception e) {
+			e.printStackTrace();
+			return false;
+		}
+	}
+}
