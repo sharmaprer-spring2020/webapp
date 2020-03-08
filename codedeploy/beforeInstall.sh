7184086955a82ee498da410fdcedecf76a6eb268
Prerna Sharma <sharma.prer@northeastern.edu>
2020-03-08 05:06:44 -0400

    Added extends

--- codedeploy/beforeInstall.sh
+++ codedeploy/beforeInstall.sh
@@ -3,11 +3,16 @@
 if [ -e /opt/tomcat8/webapps/ROOT.war ]
 then
     sudo rm /opt/tomcat8/webapps/ROOT.war
-elif [-e /opt/tomcat8/webapps/ROOT]
-then
-    sudo rm /opt/tomcat8/webapps/ROOT
 else
     echo "No ROOT/ROOT.war exists"
+fi
+
+
+if [-e /opt/tomcat8/webapps/ROOT]
+then
+    sudo rm -r /opt/tomcat8/webapps/ROOT
+else
+    echo "No ROOT/ROOT directory exists"
 fi
 
 
--- src/main/java/com/neu/edu/MainClass.java
+++ src/main/java/com/neu/edu/MainClass.java
@@ -2,12 +2,12 @@ package com.neu.edu;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class MainClass {
+public class MainClass extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(MainClass.class, args);
 	}
-
 }
