e2383d39da7656f1418a302157b651c3521db86a
Prerna Sharma <sharma.prer@northeastern.edu>
2020-03-08 03:29:32 -0400

    beforeInstall if condition

--- codedeploy/beforeInstall.sh
+++ codedeploy/beforeInstall.sh
@@ -1,4 +1,13 @@
 #!/bin/bash
 #Clean up tomcat artifacts
-sudo rm /opt/tomcat8/webapps/ROOT.war
-sudo rm /opt/tomcat8/webapps/ROOT
+if [ -e /opt/tomcat8/webapps/ROOT.war ]
+then
+    sudo rm /opt/tomcat8/webapps/ROOT.war
+elif [-e /opt/tomcat8/webapps/ROOT]
+then
+    sudo rm /opt/tomcat8/webapps/ROOT
+else
+    echo "No ROOT/ROOT.war exists"
+fi
+
+
