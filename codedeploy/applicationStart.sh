fff5dd214b93b35f5dd706f7dd11c5c2d029ab50
Prerna Sharma <sharma.prer@northeastern.edu>
2020-03-07 19:23:36 -0500

    updated with application start sh script

--- codedeploy/applicationStart.sh
+++ codedeploy/applicationStart.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 
 echo "In application Start.sh"
+sudo systemctl start tomcat.service
--- codedeploy/beforeInstall.sh
+++ codedeploy/beforeInstall.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 echo "In beforeInstall.sh"
+#!/bin/bash
 
-
+#Clean up tomcat artifacts
+sudo rm /opt/tomcat8/webapps/ROOT.war
