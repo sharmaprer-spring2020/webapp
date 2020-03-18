a750d89463a1d2ebfea6a2ed651ace8b9942c069
Prerna Sharma <sharma.prer@northeastern.edu>
2020-03-18 10:53:25 -0400

    test cloudwatch as a service

--- codedeploy/applicationStop.sh
+++ codedeploy/applicationStop.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 echo "Stop tomcat service" 
 sudo systemctl stop tomcat.service
-sudo /opt/aws/amazon-cloudwatch-agent/bin/amazon-cloudwatch-agent-ctl -m ec2 -a stop
+sudo systemctl stop cloudwatch.service
+#sudo /opt/aws/amazon-cloudwatch-agent/bin/amazon-cloudwatch-agent-ctl -m ec2 -a stop
