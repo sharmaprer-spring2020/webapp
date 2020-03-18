0649243556468f06c306da705571beb79aace442
Prerna Sharma <sharma.prer@northeastern.edu>
2020-03-18 00:27:16 -0400

    removed create dir for logs

--- codedeploy/applicationStart.sh
+++ codedeploy/applicationStart.sh
@@ -7,7 +7,7 @@ sudo /opt/aws/amazon-cloudwatch-agent/bin/amazon-cloudwatch-agent-ctl \
     -s
 echo "cloudWatch configured"
 sudo systemctl start tomcat.service && 
-mkdir /opt/tomcat8/appLogs &&
+#mkdir /opt/tomcat8/appLogs &&
 sudo chmod -R 755 /opt/tomcat8/appLogs
 
 echo "tomcat is now started"
