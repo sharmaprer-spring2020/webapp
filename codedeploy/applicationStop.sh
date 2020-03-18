1e436be20d46155e34825a2757fb768bf238e4b8
Prerna Sharma <sharma.prer@northeastern.edu>
2020-03-18 13:07:46 -0400

    remove cloudwatch stop

--- codedeploy/applicationStop.sh
+++ codedeploy/applicationStop.sh
@@ -2,4 +2,3 @@
 echo "Stop tomcat service" 
 sudo systemctl stop tomcat.service
 #sudo systemctl stop cloudwatch.service
-sudo /opt/aws/amazon-cloudwatch-agent/bin/amazon-cloudwatch-agent-ctl -m ec2 -a stop
