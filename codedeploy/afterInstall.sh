39c89d3fde25805a7bb44bd787b2ae542998bac6
Prerna Sharma <sharma.prer@northeastern.edu>
2020-03-26 21:54:40 -0400

    removed tomcat status check

--- codedeploy/afterInstall.sh
+++ codedeploy/afterInstall.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 echo "Initiate Stop tomcat service and daemon reload"
 sudo systemctl stop tomcat.service && 
-sudo systemctl daemon-reload && echo "stop daemon-reload" &&
-sudo systemctl status tomcat.service &&
+sudo systemctl daemon-reload &&
 sudo /opt/aws/amazon-cloudwatch-agent/bin/amazon-cloudwatch-agent-ctl -m ec2 -a stop
 #sudo systemctl stop cloudwatch.service
