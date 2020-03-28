aa2e4e62d4e70995b3a0eb81e9cd6736fcba3f1e
Prerna Sharma <sharma.prer@northeastern.edu>
2020-03-27 21:29:14 -0400

    while loop

--- codedeploy/applicationStart.sh
+++ codedeploy/applicationStart.sh
@@ -7,5 +7,13 @@ sudo /opt/aws/amazon-cloudwatch-agent/bin/amazon-cloudwatch-agent-ctl \
     -s
     #sudo systemctl start cloudwatch.service
 echo "cloudWatch configured"
+while [ ! -f /home/ubuntu/list.txt ]
+do
+  sleep 2 # or less like 0.2
+  echo "waiting for file"
+done
+echo "catalina.properties"
+sudo cat opt/tomcat8/conf/catalina.properties
+sudo systemctl daemon-reload
 sudo systemctl restart tomcat.service 
 echo "tomcat is now restarted"
